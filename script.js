cb681229e55ab4eca70d56c3f8ea9b582904acd8
Argonaut344619 <144302205+Argonaut344619@users.noreply.github.com>
2023-09-07 23:08:42 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const congratsText = document.getElementById("congrats");
-    let chars = congratsText.innerText.split('');
+    const sectionText = document.querySelector("section h2");
+
+    let charsCongrats = congratsText.innerText.split('');
+    let charsSection = sectionText.innerText.split('');
+
     congratsText.innerHTML = '';
-    
-    chars.forEach((char, index) => {
+    sectionText.innerHTML = '';
+
+    // <h1>のテキストを1文字ずつ表示
+    charsCongrats.forEach((char, index) => {
         setTimeout(() => {
-            congratsText.innerHTML += char;
+            congratsText.innerHTML += char === " " ? " " : (char + "💖"); // 空白の場所にはハートマークを追加しない
         }, 100 * index);
     });
+
+    // <h1>のアニメーションが終わった後に<h2>のテキストを1文字ずつ表示
+    setTimeout(() => {
+        charsSection.forEach((char, index) => {
+            setTimeout(() => {
+                sectionText.innerHTML += char === " " ? " " : (char + "🐻"); // 空白の場所にはくまさんを追加しない
+            }, 100 * index);
+        });
+    }, 100 * charsCongrats.length); // <h1>のアニメーションが完了する時間を考慮
 });
